4545b3c5a17c5eff8cb2691e53cee86d115b8166
heaven-chp <62167253+heaven-chp@users.noreply.github.com>
2024-02-05 05:27:57 +0900

    [#1] [socket] develop

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # base-server-python
+
+## Features
+ - socket
+
+<br/>
+
+## Requirements
+ - `pip install -r requirements.txt`
+
+<br/>
+
+## How to use server
+ - socket
+   - `python ./base_server/socket/__main__.py --address 0.0.0.0 --port 10000 --listen_size 10`
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="base-server",
+    version="0.0.1",
+    description="base server",
+    url="https://github.com/heaven-chp/base-server-python",
+    long_description="",
+    long_description_content_type="text/markdown",
+    author="heaven-chp",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[],
+)
